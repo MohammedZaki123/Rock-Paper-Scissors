a081dc80e4395bd58fb5d3ae65cfe254e1fc8917
MohammedZaki123 <mohammedzaki006@gmail.com>
2024-02-20 02:34:26 +0200

    After debugging and running, THE PROJECT IS DONE

--- script.js
+++ script.js
@@ -2,42 +2,59 @@ let computer_points = 0;
 let player_points = 0;
 let com_won = false;
 let player_won = false;
+const choices = ["rock", "paper", "scissors"];
 function getComputerChoice(){
-    const choices = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * choices.length);
+    let randomIndex = Math.floor(Math.random() * choices.length);
     console.log('Computer chooses ' + choices[randomIndex]);
     return choices[randomIndex];
 }
 
 function playerSelection(){
-    let input = prompt("Your TURN:");
-    console.log('player chooses ' + input);
+    let input;
+    do{
+        input = prompt("Your Turn:");
+    }
+    while(input==null || !(choices.includes(input.toLowerCase())));
+    console.log('player chooses ' + input.toLowerCase());
     return input.toLowerCase();
 }
 function playRound(playerSelection, computerSelection){
-    if(playerSelection()==computerSelection()){
+    if(playerSelection==computerSelection){
+        console.log('It is a tie' );
         return 'It is a tie';
     }
-    else if(playerSelection()=='rock' && computerSelection()=='scissors' || playerSelection()=='paper' &&
-    computerSelection()=='rock' || playerSelection()=='scissors' && computerSelection()=='paper'){
-        return "You Win! " + playerSelection() + " beats " + computerSelection();
+    else if(playerSelection=='rock' && computerSelection=='scissors' || playerSelection=='paper' &&
+    computerSelection=='rock' || playerSelection=='scissors' && computerSelection=='paper'){
+        console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        return "You Win! " + playerSelection + " beats " + computerSelection;
     }else{ 
-        return 'You Lose! ' + computerSelection() + " beats " + playerSelection();
+        console.log('You Lose! ' + computerSelection + " beats " + playerSelection);
+        return 'You Lose! ' + computerSelection + " beats " + playerSelection;
     }
 }
 function playGame(){
     let round_no = 1
-    let round_winner = '';
+    let round_play = '';
+    let a_tie = false;
     while (round_no <= 5){
-        round_winner = playRound(playerSelection, getComputerChoice);
-        if(round_winner.includes('Lose')){
+        if(!a_tie){
+        console.log("round number: " + round_no);
+        }
+        let player = playerSelection(); 
+        let computer = getComputerChoice();
+        round_play = playRound(player, computer);
+        if(round_play.includes('Lose')){
             computer_points++;
-        }else{
+        }else if(round_play.includes('Win')){
             player_points++;
 
+        }else{
+            a_tie = true;
+            continue;
         }
-        console.log('Current Score: '+ computer_points + " - "+ player_points);
+        console.log('Current Score: '+ computer_points + "(for Computer) - "+ player_points + " for Player");
         round_no++;
+        a_tie = false;
     }
 }
 
@@ -48,15 +65,12 @@ function checkTheWinner(){
         com_won = true;
     }
 }
-// playGame();
-// checkTheWinner();
-// if (com_won){
-//     console.log('Game Over!!!!, Computer won');
-//     console.log('Final Score: '+ computer_points + " - "+ player_points);
-// }else if(player_won){
-//     console.log('Game Over!!!!, Player won');
-//     console.log('Final Score: '+ player_points + " - "+ computer_points);
-// }
-// let player = 'paper';
-// let Computer = 'scissors'
-console.log(playRound(playerSelection, getComputerChoice));
+playGame();
+checkTheWinner();
+if (com_won){
+    console.log('Game Over!!!!, Computer won');
+    console.log('Final Score: '+ computer_points + " - "+ player_points);
+}else if(player_won){
+    console.log('Game Over!!!!, Player won');
+    console.log('Final Score: '+ player_points + " - "+ computer_points);
+}
